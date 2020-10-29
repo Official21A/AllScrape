3590337b1ca0cb0b520fb78014402b6ff057ade4
Official21A <officialamirhossein21@gmail.com>
2020-10-29 08:36:56 +0330

    Follow

--- top_leauges/top_leauges/spiders/quotes_spider.py
+++ top_leauges/top_leauges/spiders/quotes_spider.py
@@ -25,6 +25,18 @@ class QuotesSpider(scrapy.Spider): # the spider class to scrape the url page
             f.write(response.body)
         self.log(f'Saved file {filename}')
 
+    def other_parse(self, response):
+    	for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('span small::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
+
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
 # a usefull way to scrape and get the data you want is to use
 # >> scrapy shell 'url_domain_name'  
 # this will open the scrapy shell for testing and getting the data.      
