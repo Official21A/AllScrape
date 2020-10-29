ff64a4f3fbf48987d42ddb2025a8ed239ef45c13
Official21A <officialamirhossein21@gmail.com>
2020-10-29 08:45:46 +0330

    Refactor

--- top_leauges/top_leauges/spiders/author_spider.py
+++ top_leauges/top_leauges/spiders/author_spider.py
@@ -4,9 +4,10 @@ import scrapy
 class AuthorSpider(scrapy.Spider): # an other class for example
     name = "author" # class name
 
-    start_urls = ['http://quotes.toscrape.com/']
+    start_urls = ['http://quotes.toscrape.com/'] # web urls , defined directly
 
     def parse(self, response):
+        # to follow all the a tags
         author_page_links = response.css('.author + a')
         yield from response.follow_all(author_page_links, self.parse_author)
 
@@ -14,6 +15,7 @@ class AuthorSpider(scrapy.Spider): # an other class for example
         yield from response.follow_all(pagination_links, self.parse)
 
     def parse_author(self, response):
+        # a method for getting the page and take the data we want from it
         def extract_with_css(query):
             return response.css(query).get(default='').strip()
 
